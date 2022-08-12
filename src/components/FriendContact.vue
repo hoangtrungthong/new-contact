b6674d643fc4a82b3f4e48b6d962d60af1039e2a
hoangtrungthong <hoangtrungthong0000@gmail.com>
2022-08-12 14:30:01 +0700

    asd

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,49 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">toggle favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="$emit('delete', id)">delete</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    "name",
+    "phoneNumber",
+    "email",
+    "isFavorite",
+    "id"
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      }
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id)
+    }
+  }
+};
+</script>
